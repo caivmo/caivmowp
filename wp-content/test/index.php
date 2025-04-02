f26dbafb6aefd53053e4e0fdd9ef16d135302129
ivan moreno <carlosivanmorenoc@gmail.com>
2025-04-01 22:06:23 -0500

    Validate

--- wp-content/test/index.php
+++ wp-content/test/index.php
@@ -1,6 +1,18 @@
 <?php
 
-echo "Hello World!";
- if (isset($_GET['name'])) {
-    echo "Hello " . $_GET['name'];
+// Syntax error (missing semicolon)
+echo "Hello, WordPress!"
+
+// Undefined variable
+echo $undefinedVar;
+
+// Function with incorrect parameter usage
+function addNumbers(int $a, int $b) {
+    return $a + $b;
 }
+
+// Calling function with incorrect types (should be integers)
+$result = addNumbers("10", "20");
+
+// Using an undefined function
+nonExistentFunction();
