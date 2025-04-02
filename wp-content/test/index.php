ba492e818a0dd412c6c60ec6a21ac13aff24e0b3
ivan moreno <carlosivanmorenoc@gmail.com>
2025-04-02 00:04:49 -0500

    Update

--- wp-content/test/index.php
+++ wp-content/test/index.php
@@ -1,2 +1,15 @@
 <?php
-echo "Hello World!";
+/**
+ * Index file for WordPress
+ *
+ * @package WordPress
+ * @since 1.0.0
+ * @version 1.0.0
+ * @author Ivan
+ * @copyright 2023 Ivan
+ * @license GPL-2.0-or-later
+ * @link https://www.gnu.org/licenses/gpl-2.0.html
+ * @description This is a test file for WordPress coding standards.
+ */
+
+echo 'Hello World!';
--- wp-content/test/test.php
+++ wp-content/test/test.php
@@ -1,25 +1,15 @@
 <?php
-// index.php con malos estándares de codificación para WordPress
+/**
+ * Index file for WordPress
+ *
+ * @package WordPress
+ * @since 1.0.0
+ * @version 1.0.0
+ * @author Ivan
+ * @copyright 2023 Ivan
+ * @license GPL-2.0-or-later
+ * @link https://www.gnu.org/licenses/gpl-2.0.html
+ * @description This is a test file for WordPress coding standards.
+ */
 
-function testfunction() {
-	echo 'Hola Mundo';} // Falta de indentación y espaciado adecuado
-
-testfunction();
-
-class myClass {
-	function __construct() {
-		$this->doSomething();} // El nombre de la clase debería estar en mayúscula y métodos en minúscula, además de la indentación
-	function doSomething() {
-		echo 'Haciendo algo...';} // No hay separación ni comentarios, ni documentación
-}
-
-$obj = new myClass();
-
-if ( isset( $_GET['id'] ) ) {
-	$id = $_GET['id'];
-} else {
-	$id = 0;} // Falta de espacios y sangrías en la estructura de control
-
-// Código adicional sin comentarios ni documentación
-
-echo 'El ID es: ' . $id;
+echo 'Hello World!';
