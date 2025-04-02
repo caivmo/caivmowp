a5dfa52fb1ae67d52aed3118cb7143c976c9a396
ivan moreno <carlosivanmorenoc@gmail.com>
2025-04-01 23:07:10 -0500

    Update

--- wp-content/test/index.php
+++ wp-content/test/index.php
@@ -1,15 +1,25 @@
 <?php
+// index.php con malos estándares de codificación para WordPress
 
-	// This line uses a tab for indentation (BAD)
-	echo "Hello, WordPress!";  
-      
-// This line uses spaces for indentation (BAD)
-    echo "This line is indented with spaces";
+function testfunction() {
+	echo 'Hola Mundo';} // Falta de indentación y espaciado adecuado
 
-// Mixed indentation (tab + spaces)
-	echo "This line starts with a tab, then spaces";    
+testfunction();
 
-// Extra trailing spaces at the end of lines      
-echo "This has trailing spaces";         
+class myClass {
+	function __construct() {
+		$this->doSomething();} // El nombre de la clase debería estar en mayúscula y métodos en minúscula, además de la indentación
+	function doSomething() {
+		echo 'Haciendo algo...';} // No hay separación ni comentarios, ni documentación
+}
 
-?>
+$obj = new myClass();
+
+if ( isset( $_GET['id'] ) ) {
+	$id = $_GET['id'];
+} else {
+	$id = 0;} // Falta de espacios y sangrías en la estructura de control
+
+// Código adicional sin comentarios ni documentación
+
+echo 'El ID es: ' . $id;
